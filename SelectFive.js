67fd132a42c94783be8e00ac9e5cf24df6f411b1
Hussain Rifad <hussain.rifad89@gmail.com>
2022-08-23 13:33:45 +0600

    shadow added to card

---
+++ SelectFive.js
@@ -0,0 +1,96 @@
+// _______Function for Disable Button_________ 
+
+function DisableNextButton(btnId) {
+    document.getElementById(btnId).disabled = 'true';
+}
+
+// _______Function for Add player in the list_________ 
+
+function clickhandler(buttonClick, listsInUl, playerInnerText) {
+
+    document.getElementById(buttonClick).addEventListener('click',function(){
+
+        const addlist = document.getElementById(listsInUl);
+        const li = document.createElement('li');
+        const name = document.getElementById(playerInnerText);
+        const playerNAme = name.innerText;
+        li.innerText = playerNAme;
+        console.log(addlist.getElementsByTagName('li'));
+
+
+        // ___________Check if list not larger than 5________________
+
+
+        if (addlist.getElementsByTagName('li').length < 5 ) {
+            
+
+            addlist.appendChild(li);
+
+        }
+
+        // _______________Calling the disable button function____________
+
+        DisableNextButton(buttonClick);
+    
+    })
+
+}
+
+// _____________________Function for total player cost_____________________ 
+
+function TotalPlayerCost(buttonPlayer, eachPlayerCost, displayAmount)
+{
+
+    document.getElementById(buttonPlayer).addEventListener('click', function(){
+        const inputAmount = document.getElementById(eachPlayerCost);
+        const value = inputAmount.value;
+    
+        const sum = (5*value);
+        const display = document.getElementById(displayAmount);
+        display.innerText = sum;
+    })
+
+}
+
+
+//______________________Calling the function_________________________
+
+clickhandler('button-messi', 'list-five', 'playernameMessi');
+clickhandler('button-ramos', 'list-five', 'playernameRamos');
+clickhandler('button-hakimi', 'list-five', 'playernameHakimi');
+clickhandler('button-Donnarumma', 'list-five', 'playernameDonnarumma');
+clickhandler('button-kurzawa', 'list-five', 'playernameKurzawa');
+clickhandler('button-paredes', 'list-five', 'playernameParedes');
+clickhandler('button-marquinhos', 'list-five', 'playernameMarquinhos');
+clickhandler('button-verratti', 'list-five', 'playernameVerratti');
+clickhandler('button-neymar', 'list-five', 'playernameNeymar');
+clickhandler('button-mbappe', 'list-five', 'playernameMbappe');
+clickhandler('button-sanches', 'list-five', 'playernameSanches');
+clickhandler('button-vitinha', 'list-five', 'playernameVitinha');
+
+TotalPlayerCost('playerCalculation', 'playerCost', 'displaycost');
+
+
+
+// _____________________________Calculating the Total cost______________________________
+
+
+document.getElementById('totalCal').addEventListener('click', function(){
+
+    const manager = document.getElementById('managerValue');
+    const coach = document.getElementById('coachValue');
+
+    const managervalue = manager.value;
+    const coachvalue = coach.value;
+
+    const inputAmount = document.getElementById('playerCost');
+    const value = inputAmount.value;
+    
+    const sum = (5*value);
+
+    const total = sum + parseInt(managervalue) + parseInt(coachvalue) ;
+
+    const Display = document.getElementById('displaytotal');
+    Display.innerText = total;
+
+})
